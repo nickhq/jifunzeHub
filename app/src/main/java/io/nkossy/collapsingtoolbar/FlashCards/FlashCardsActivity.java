e17fb21709ad126dc7f5e90932496d0c7c526918
Nixon Kosgei <nkosgey6@gmail.com>
2018-02-16 05:32:44 +0300

    removed collapsing toolbar in favor of a normal toolbar in order to support navigation drawer

--- app/src/main/java/io/nkossy/collapsingtoolbar/FlashCards/FlashCardsActivity.java
+++ app/src/main/java/io/nkossy/collapsingtoolbar/FlashCards/FlashCardsActivity.java
@@ -1,19 +1,44 @@
 package io.nkossy.collapsingtoolbar.FlashCards;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import io.nkossy.collapsingtoolbar.NavigationActivity;
 import io.nkossy.collapsingtoolbar.R;
 
-public class FlashCardsActivity extends AppCompatActivity {
+public class FlashCardsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash_cards);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle.syncState();
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         CardView cardBiology = findViewById(R.id.card_biology);
 
@@ -23,5 +48,51 @@ public class FlashCardsActivity extends AppCompatActivity {
                 startActivity(new Intent(getBaseContext(), BiologyFlashCardActivity.class));
             }
         });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.nav_flash_cards:
+                Toast.makeText(this, "You are here", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_quiz:
+                startActivity(new Intent(this, NavigationActivity.class));
+                break;
+            case R.id.nav_leader_board:
+                Toast.makeText(this, "Coming soon", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_setting:
+                Toast.makeText(this, "Coming Soon", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this, "Coming Soon", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_feedback:
+                Toast.makeText(this, "Coming Soon", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_help:
+                Toast.makeText(this, "Coming Soon", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_about:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                        .setTitle("About Developers")
+                        .setMessage("We Students Building Android Apps for Students")
+                        .setIcon(R.drawable.ic_info_24dp)
+                        .setCancelable(true)
+                        .setPositiveButton("Close", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // leave this empty
+                            }
+                        });
+                builder.show();
+                break;
+        }
+
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
